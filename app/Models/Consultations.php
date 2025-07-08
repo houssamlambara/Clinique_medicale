c922dee81633c72ba92d1ddee00fc8f614258523
houssamlambara <lambarahoussam@gmail.com>
2025-07-08 23:36:12 +0100

    creation et migration du tables

---
+++ app/Models/Consultations.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Consultations extends Model
+{
+    //
+}
---
+++ database/migrations/2025_07_08_220344_create_rendezvous_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rendezvous', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('patient_id')->constrained();
+            $table->foreignId('medecin_id ')->constrained();
+            $table->date("date_rdv");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rendezvouses');
+    }
+};
