4ca3b02dbcc627feae3cd4b235ddeb2b19177ed3
houssamlambara <lambarahoussam@gmail.com>
2025-07-13 16:26:19 +0100

    provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,8 +5,16 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use App\Interfaces\IPatientRepository;
 use App\Interfaces\IDossierMedicalRepository;
+use App\Interfaces\IPrescriptionRepository;
+use App\Interfaces\IRendezvousRepository;
+use App\Interfaces\IMedecinRepository;
+use App\Interfaces\IInfirmierRepository;
 use App\Repositories\PatientRepository;
 use App\Repositories\DossierMedicalRepository;
+use App\Repositories\PrescriptionRepository;
+use App\Repositories\RendezvousRepository;
+use App\Repositories\MedecinRepository;
+use App\Repositories\InfirmierRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
         // Injection de dépendance pour les repositories
         $this->app->bind(IPatientRepository::class, PatientRepository::class);
         $this->app->bind(IDossierMedicalRepository::class, DossierMedicalRepository::class);
+        $this->app->bind(IPrescriptionRepository::class, PrescriptionRepository::class);
+        
     }
 
     /**
